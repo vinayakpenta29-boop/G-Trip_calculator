ff8d870c742d25b778330ae524fe23e63573faf9
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2026-05-17 21:33:11 +0530

    Upgrade database version and add members table

--- app/src/main/java/com/example/tripexpense/DatabaseHelper.java
+++ app/src/main/java/com/example/tripexpense/DatabaseHelper.java
@@ -11,12 +11,19 @@ import java.util.List;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "expenses.db";
-    private static final int DATABASE_VERSION = 1;
-    
-    public static final String TABLE_NAME = "expense_table";
-    public static final String COL_ID = "ID";
-    public static final String COL_TITLE = "TITLE";
-    public static final String COL_AMOUNT = "AMOUNT";
+    private static final int DATABASE_VERSION = 2; // Upgraded version!
+
+    // Member Table
+    public static final String TABLE_MEMBERS = "members_table";
+    public static final String COL_MEMBER_ID = "ID";
+    public static final String COL_MEMBER_NAME = "NAME";
+
+    // Expense Table
+    public static final String TABLE_EXPENSES = "expense_table";
+    public static final String COL_EXP_ID = "ID";
+    public static final String COL_EXP_TITLE = "TITLE";
+    public static final String COL_EXP_AMOUNT = "AMOUNT";
+    public static final String COL_EXP_PAYER_ID = "PAYER_ID";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -24,42 +31,72 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createTable = "CREATE TABLE " + TABLE_NAME + " (" +
-                COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                COL_TITLE + " TEXT, " +
-                COL_AMOUNT + " REAL)";
-        db.execSQL(createTable);
+        String createMembersTable = "CREATE TABLE " + TABLE_MEMBERS + " (" +
+                COL_MEMBER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL_MEMBER_NAME + " TEXT)";
+        
+        String createExpensesTable = "CREATE TABLE " + TABLE_EXPENSES + " (" +
+                COL_EXP_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL_EXP_TITLE + " TEXT, " +
+                COL_EXP_AMOUNT + " REAL, " +
+                COL_EXP_PAYER_ID + " INTEGER)";
+                
+        db.execSQL(createMembersTable);
+        db.execSQL(createExpensesTable);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_MEMBERS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_EXPENSES);
+        // Also drop the old v1 table name just in case
+        db.execSQL("DROP TABLE IF EXISTS expense_table"); 
         onCreate(db);
     }
 
-    public boolean insertExpense(String title, double amount) {
+    // --- Member Methods ---
+    public boolean insertMember(String name) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_TITLE, title);
-        contentValues.put(COL_AMOUNT, amount);
-        long result = db.insert(TABLE_NAME, null, contentValues);
+        contentValues.put(COL_MEMBER_NAME, name);
+        long result = db.insert(TABLE_MEMBERS, null, contentValues);
+        return result != -1;
+    }
+
+    public List<Member> getAllMembers() {
+        List<Member> list = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_MEMBERS, null);
+        if (cursor.moveToFirst()) {
+            do {
+                list.add(new Member(cursor.getInt(0), cursor.getString(1)));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        return list;
+    }
+
+    // --- Expense Methods ---
+    public boolean insertExpense(String title, double amount, int payerId) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_EXP_TITLE, title);
+        contentValues.put(COL_EXP_AMOUNT, amount);
+        contentValues.put(COL_EXP_PAYER_ID, payerId);
+        long result = db.insert(TABLE_EXPENSES, null, contentValues);
         return result != -1;
     }
 
     public List<Expense> getAllExpenses() {
-        List<Expense> expenseList = new ArrayList<>();
-        SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
-
+        List<Expense> list = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_EXPENSES, null);
         if (cursor.moveToFirst()) {
             do {
-                int id = cursor.getInt(0);
-                String title = cursor.getString(1);
-                double amount = cursor.getDouble(2);
-                expenseList.add(new Expense(id, title, amount));
+                list.add(new Expense(cursor.getInt(0), cursor.getString(1), cursor.getDouble(2), cursor.getInt(3)));
             } while (cursor.moveToNext());
         }
         cursor.close();
-        return expenseList;
+        return list;
     }
 }
