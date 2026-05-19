93cfbd119b778eec5f4506ff3e9d72560d115d8d
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2026-05-19 23:20:48 +0530

    Update Member.java

--- app/src/main/java/com/example/tripexpense/Member.java
+++ app/src/main/java/com/example/tripexpense/Member.java
@@ -1,18 +1,21 @@
 package com.example.tripexpense;
 
 public class Member {
-    private int id;
+    private String id;
     private String name;
 
-    public Member(int id, String name) {
+    // 1. REQUIRED BY FIREBASE
+    public Member() {} 
+
+    // 2. Standard Constructor
+    public Member(String id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public int getId() { return id; }
+    public String getId() { return id; }
     public String getName() { return name; }
 
-    // This makes the Spinner (dropdown menu) display the person's name
     @Override
     public String toString() {
         return name;
