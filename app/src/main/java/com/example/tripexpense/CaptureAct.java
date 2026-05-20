fe39ee7fcc63fedc822c9f462f1bc3a283f31441
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2026-05-20 08:59:16 +0530

    Create CaptureAct.java

---
+++ app/src/main/java/com/example/tripexpense/CaptureAct.java
@@ -0,0 +1,7 @@
+package com.example.tripexpense;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity {
+    // Leave this completely empty! 
+}
