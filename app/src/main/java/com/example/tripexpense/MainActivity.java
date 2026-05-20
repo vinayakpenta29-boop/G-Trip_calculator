2ca7e2ea0a53998f064dc946660ec4abca8d20a1
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2026-05-21 02:06:34 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/tripexpense/MainActivity.java
+++ app/src/main/java/com/example/tripexpense/MainActivity.java
@@ -518,4 +518,28 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+        // --- UI HELPERS ---
+    
+    private void setDynamicListViewHeight(ListView listView) {
+        ListAdapter adapter = listView.getAdapter();
+        if (adapter == null) {
+            return;
+        }
+
+        int totalHeight = 0;
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.UNSPECIFIED);
+
+        for (int i = 0; i < adapter.getCount(); i++) {
+            View listItem = adapter.getView(i, null, listView);
+            listItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (adapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
+    }
+
+
 }
